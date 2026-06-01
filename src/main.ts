53463cd2942ba7988b7ea1154bf390aeb6c5e95f
DhamayLahutagistha <dhamay_putri_33rpl@student.smktelkom-mlg.sch.id>
2026-06-01 21:03:22 +0700

    Use Railway PORT

--- src/main.ts
+++ src/main.ts
@@ -57,7 +57,7 @@ async function bootstrap() {
     swaggerOptions: { persistAuthorization: true },
   });
 
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 8080;
   await app.listen(port);
 
   console.log(`🚀 Server running on http://localhost:${port}`);
